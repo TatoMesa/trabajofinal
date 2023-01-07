bd8295811cc02ae2deeeb052645d2310ae9a0783
TatoMesa <seba_mesa@hotmail.com>
2023-01-07 12:36:13 -0300

    primer commit laravel9

---
+++ app/Http/Controllers/NoticiaController.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\noticia;
+use Illuminate\Http\Request;
+
+class NoticiaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        return view('noticia.index');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+        return view('noticia.create');
+
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\noticia  $noticia
+     * @return \Illuminate\Http\Response
+     */
+    public function show(noticia $noticia)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\noticia  $noticia
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(noticia $noticia)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\noticia  $noticia
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, noticia $noticia)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\noticia  $noticia
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(noticia $noticia)
+    {
+        //
+    }
+}
